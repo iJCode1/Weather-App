f7c9725fb2334004cb1abce6f13ed8f4a35b607d
Joel <joeldome17@gmail.com>
2022-01-11 21:05:56 -0600

    Ejercicio con bucles

---
+++ js/ejercicios.js
@@ -0,0 +1,7 @@
+//Plana de nVeces: No debo olvidar lo aprendido en Javascript
+const plana = (nVeces)=>{
+  for(let i = 1; i<= nVeces; i = i+1){
+    console.log(`${i}. No debo olvidar lo aprendido en Javascript`);
+  }
+}
+plana(170);

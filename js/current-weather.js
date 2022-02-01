fbbd817c43b4278e4f27024755ce781248741330
Joel <joeldome17@gmail.com>
2022-01-31 21:39:38 -0600

    Renderizando temperatura actual

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,17 +1,37 @@
 import weather from '../data/current-weather.js';
+import {formatDate} from './utils/format-data.js';
+import {formatTemp} from './utils/format-data.js';
 
 function setCurrentCity($elemento, city){
   $elemento.textContent = city;
 }
-
+const config = {
+  day: "numeric",
+  weekday: "long",
+  month: "long",
+}
+function setCurrentDate($elemento){
+  const date = new Date();
+  const formattedDate = formatDate(date);
+  $elemento.textContent = formattedDate;
+}
+function setCurrentTemp($elemento, temp){
+  const formattedTemp = formatTemp(temp);
+  $elemento.textContent = formattedTemp;
+}
 function configCurrentWeather(weather){
   //loader
   //date
+  const $currentWeatherDate = document.querySelector("#current-weather-date");
+  setCurrentDate($currentWeatherDate);
   //city
   const city = weather.name;
   const $currentWeatherCity = document.querySelector("#current-weather-city");
   setCurrentCity($currentWeatherCity, city);
   //temp
+  const $currentWeatherTemp = document.querySelector("#current-weather-temp");
+  const temp = weather.main.temp;
+  setCurrentTemp($currentWeatherTemp, temp);
   //background
 }
 
---
+++ js/utils/format-data.js
@@ -0,0 +1,11 @@
+const defaultDateOptions = {
+  day: "numeric",
+  weekday: "long",
+  month: "long",
+}
+export function formatDate(date, options = defaultDateOptions){
+  return new Intl.DateTimeFormat('es', options).format(date);
+}
+export function formatTemp(temp){
+  return `${Math.floor(temp)}°`
+}

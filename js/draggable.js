14a07eb57332ecf674c11354f6de2abc064cd0fe
Joel <joeldome17@gmail.com>
2022-02-22 19:22:48 -0600

    Definiendo elemento arrastrable y estado inicial del widget

---
+++ js/draggable.js
@@ -0,0 +1,45 @@
+const defaultConfig = {
+  open: true,
+  debug: true,
+  animatable: true,
+}
+export default function draggable($element, config = defaultConfig){
+  if(!($element instanceof HTMLElement)){
+    return console.warn(`Elemento invalido, se esperabá un HTMLElement y se recibió ${$element}`);
+  }
+  let isOpen = config.open;
+  const elementRect = $element.getBoundingClientRect();
+  const ELEMENT_BLOCK_SIZE = elementRect.height;
+  
+  const $marker = $element.querySelector('[data-marker]');
+  const MARKER_BLOCK_SIZE = $marker.getBoundingClientRect().height;
+
+  const VISIBLE_Y_POSITION = 0;
+  const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE;
+  let widgetPosition = VISIBLE_Y_POSITION;
+  
+  isOpen ? open() : close();
+
+  function logger(message){
+    if(config.debug){
+      console.info(message);
+    }
+  }
+
+  function open(){
+    logger('Abrir Widget');
+    isOpen = true;
+    widgetPosition = VISIBLE_Y_POSITION;
+    setWidgetPosition(widgetPosition);
+  }
+  function close(){
+    logger('Cerrar Widget');
+    isOpen = false;
+    widgetPosition = HIDDEN_Y_POSITION;
+    setWidgetPosition(widgetPosition);
+  }
+
+  function setWidgetPosition(value){
+    $element.style.marginBottom = `-${value}px`;
+  }
+}
--- js/weekly-weather.js
+++ js/weekly-weather.js
@@ -3,6 +3,7 @@ import {getLanLon} from './geolocation.js';
 import {formatWeekList} from './utils/format-data.js';
 import {createDOM} from './utils/dom.js';
 import {createPeriodtime} from './period-time.js';
+import draggable from './draggable.js';
 
 function tabPanelTemplate(id){
   return `
@@ -33,10 +34,12 @@ function configWeeklyWeather(weekList){
   });
 }
 export default async function weeklyWeather(){
+  const $container = document.querySelector('.weeklyWeather');
   const {lat, lon, isError} = await getLanLon();
   if(isError) return console.log('A ocurrido un error ubicandote');
   const {isError:weeklyWeatherError, data:weather} = await getWeeklyWeather(lat, lon);
   if(weeklyWeatherError) return console.log('oh! A ocurrido un error trayendo el pronóstico del clima');
   const weekList = formatWeekList(weather.list);
   configWeeklyWeather(weekList);
+  draggable($container);
 }
